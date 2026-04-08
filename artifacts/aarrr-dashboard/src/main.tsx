eb971c58ed392680e3a2bf9a0d699809fcd992c8
Emma0077 <emma.lguplus@gmail.com>
2026-04-08 23:04:21 +0000

    Update main.tsx

--- artifacts/aarrr-dashboard/src/main.tsx
+++ artifacts/aarrr-dashboard/src/main.tsx
@@ -1,17 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
-console.log("main.tsx loaded");
+createRoot(document.getElementById("root")!).render(<App />);
 
-const root = document.getElementById("root");
-
-if (!root) {
-  throw new Error("#root not found");
-}
-
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+main.tsx
